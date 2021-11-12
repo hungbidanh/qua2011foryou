7ffa030aff9a53f0513db19f700a91d885f49534
hungbidanh <88775787+hungbidanh@users.noreply.github.com>
2021-11-12 09:59:02 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em Yêu';
+const giftUrl = 'https://hungbidanh.github.io/';
+const eventName = 'Chúc Mừng 20-11';
+const titleCard = 'Tặng công chúa của tui';
+const contentCard = 'Chúc cô giáo tương lai 20/11 tràn ngập niềm vui và những nụ cười. Chúc bé luôn xinh đẹp, tự tin và thành công trong mọi việc nhé <3 ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'công chúa.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
