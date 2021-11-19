96121de2f2940c9146bea1d850bb844c70c0716a
hungbidanh <88775787+hungbidanh@users.noreply.github.com>
2021-11-19 14:57:10 +0700

    Thay tui thành anh

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const nameGirl = 'Em Yêu';
 const giftUrl = 'https://hungbidanh.github.io/';
 const eventName = 'Chúc Mừng 20-11';
-const titleCard = 'Tặng công chúa của tui';
+const titleCard = 'Tặng công chúa của anh';
 const contentCard = 'Chúc cô giáo tương lai 20/11 tràn ngập niềm vui và những nụ cười. Chúc bé luôn xinh đẹp, tự tin và thành công trong mọi việc nhé <3 ';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
